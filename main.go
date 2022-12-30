586512641377f83628d737c142c13e37a2cffbb2
Antdanila <ant-danila@yandex.ru>
2022-12-30 20:07:28 +0300

    sozd

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/jung-kurt/gofpdf"
+)
+
+func main() {
+
+	///var pts []gofpdf.PointType
+
+	pdf := gofpdf.New("P", "mm", "A4", "") // A4 210.0 x 297.0
+	pdf.AddPage()
+	//pdf.SetDrawColor(50, 50, 50)
+	//pdf.SetFont("Arial", "B", 16)
+
+	pts := []gofpdf.PointType{
+		{X: 0, Y: 0},
+		{X: 0, Y: 100},
+		{X: 100, Y: 100},
+		{X: 100, Y: 0},
+	}
+
+	pdf.Polygon(pts, "D")
+
+	fileStr := "hello.pdf"
+	//pdf.OpenLayerPane()
+	//pdf.OutputFileAndClose(fileStr)
+	//example.Summary(err, fileStr)
+	pdf.Output()
+}
