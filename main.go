aed9a61ba6cb629a1954b5d195de45309ba5ac34
ant-d1 <ant-d1@Danil.MyMiFi>
2023-01-05 21:10:56 +0300

    1

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
-	api "github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/pdfcpu/pdfcpu/pkg/api"
 	pdfcpu "github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
 )
 
@@ -32,10 +34,22 @@ func main() {
 
 	// pdfcpu stamp add -mode text -- "Some multi\nline text"
 	// "ma:5, bo:7 round .3 .7 .7, fillc:#3277d3, bgcol:#beded9, rot:0" in.pdf out.pdf
-	//"font:Helvetica, points:24, rtl:off, sc:0.5 rel, pos:c, off:0 0, align:c, fillc:#808080, strokec:#808080, rot:0, op:1, mo:0, ma:5, bo:7 round .3 .7 .7, bgcol:#beded9"
+	// "font:Helvetica, points:24, rtl:off, sc:0.5 rel, pos:c, off:0 0, align:c, fillc:#808080, strokec:#808080, rot:0, op:1, mo:0, ma:5, bo:7 round .3 .7 .7, bgcol:#beded9"
 
-	wm, err = api.TextWatermark("test 2 test 2",
-		"ma:0, bo:0, rot:0",
+	// wm, err = api.TextWatermark("test 2 test 2",
+	// 	"ma:0, bo:0, rot:0",
+	// 	onTop,
+	// 	false,
+	// 	unit)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//api.AddWatermarksFile(inFile, outFile, []string{"1"}, wm, nil)
+
+	wm, err = api.TextWatermark("test 2 test 2"+"\n"+"dop str",
+		"font:Helvetica, points:24, rtl:off, sc:0.5 rel, pos:c, off:0 0, align:c, fillc:#808080, strokec:#808080, rot:0, op:1, mo:0, ma:5, bo:7 round .3 .7 .7, bgcol: 1.0 1.0 1.0",
 		onTop,
 		false,
 		unit)
@@ -44,6 +58,26 @@ func main() {
 		log.Fatal(err)
 	}
 
-	api.AddWatermarksFile(inFile, outFile, []string{"1"}, wm, nil)
+	Readers, err := os.Open(inFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	Writes, err := os.Create(outFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	api.AddWatermarks(Readers, Writes, []string{"1"}, wm, nil)
+
+	// pb, err := api.Box("20% 40%", unit)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	//api.Crop(Readers, Writes, []string{"1"}, pb, nil)
+
+	defer Readers.Close()
+	defer Writes.Close()
 
 }
