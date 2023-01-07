2cfef6dce8cedef70bef1d19da8a8beaf7e63672
ant-d1 <ant-d1@Danil.MyMiFi>
2023-01-07 18:16:24 +0300

    1

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ func main() {
 		log.Fatal(err)
 	}
 
+	//dx, dy
 	wm, err = api.TextWatermark("test 2 test 2"+"\n"+"dop str",
 		"font:Helvetica, points:24, rtl:off, sc:0.5 rel, pos:br, off:0 0, align:c, fillc: 0 0 0, strokec: 0 0 0, rot:0, op:1, mo:0, ma:5, bo:7 round .0 .0 .0, bgcol: 1.0 1.0 1.0",
 		onTop,
