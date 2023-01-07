40f371162b11350f0f1caee8a8ffaf585fe25766
ant-d1 <ant-d1@Danil.MyMiFi>
2023-01-07 18:02:40 +0300

    1

--- main.go
+++ main.go
@@ -20,36 +20,12 @@ func main() {
 
 	onTop := true
 
-	// wm, err = api.TextWatermark("test 1",
-	// 	"font:Courier, points:48, col: 1 0 0, rot:0, sc:1 abs",
-	// 	onTop,
-	// 	false,
-	// 	unit)
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// api.AddWatermarksFile(inFile, outFile, []string{"1"}, wm, nil)
-
-	// pdfcpu stamp add -mode text -- "Some multi\nline text"
-	// "ma:5, bo:7 round .3 .7 .7, fillc:#3277d3, bgcol:#beded9, rot:0" in.pdf out.pdf
-	// "font:Helvetica, points:24, rtl:off, sc:0.5 rel, pos:c, off:0 0, align:c, fillc:#808080, strokec:#808080, rot:0, op:1, mo:0, ma:5, bo:7 round .3 .7 .7, bgcol:#beded9"
-
-	// wm, err = api.TextWatermark("test 2 test 2",
-	// 	"ma:0, bo:0, rot:0",
-	// 	onTop,
-	// 	false,
-	// 	unit)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//api.AddWatermarksFile(inFile, outFile, []string{"1"}, wm, nil)
-
 	wm, err = api.TextWatermark("test 2 test 2"+"\n"+"dop str",
-		"font:Helvetica, points:24, rtl:off, sc:0.5 rel, pos:c, off:0 0, align:c, fillc: 0 0 0, strokec: 0 0 0, rot:0, op:1, mo:0, ma:5, bo:7 round .0 .0 .0, bgcol: 1.0 1.0 1.0",
+		"font:Helvetica, points:24, rtl:off, sc:0.5 rel, pos:br, off:0 0, align:c, fillc: 0 0 0, strokec: 0 0 0, rot:0, op:1, mo:0, ma:5, bo:7 round .0 .0 .0, bgcol: 1.0 1.0 1.0",
 		onTop,
 		false,
 		unit)
@@ -69,13 +45,6 @@ func main() {
 	}
 
 	api.AddWatermarks(Readers, Writes, []string{"1"}, wm, nil)
-
-	// pb, err := api.Box("20% 40%", unit)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	//api.Crop(Readers, Writes, []string{"1"}, pb, nil)
 
 	defer Readers.Close()
 	defer Writes.Close()
